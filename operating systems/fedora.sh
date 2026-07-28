ed070a8d1505e7c6ebfb640f52ee4d11f923384f
Martian <73861544+Martian1337@users.noreply.github.com>
2026-07-28 15:02:42 -0500

    Remove Lokinet installation steps from fedora.sh
    Removed Lokinet installation from Fedora setup script.

--- operating systems/fedora.sh
+++ operating systems/fedora.sh
@@ -69,11 +69,6 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | tee /etc/yum.repos.d/vscodium.repo
 dnf install -y codium
 
-# Lokinet
-echo "[+] Installing Lokinet..."
-dnf config-manager addrepo --from-repofile=https://rpm.oxen.io/fedora/oxen.repo || true
-dnf install -y lokinet
-systemctl enable lokinet --now
 
 # 2. Virtualization Stack & Advanced Hardening (Libvirt/KVM)
 echo "[+] Installing and Hardening Virtualization Stack..."
